dc3f46d5ba40706333a387e6256e931763698ee5
KekarLamar <jcarlos.demarcos@gmail.com>
2024-03-01 17:12:20 +0100

    Retomando los apuntes

---
+++ Beginners.py
@@ -0,0 +1,13 @@
+import random
+
+def get_choices():
+
+    player_choice = input ("Enter a choice (rock, paper, scissors: ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = "rock"
+    choices = {"Player": player_choice , "Computer" : computer_choice}
+    return choices
+
+choices = get_choices()
+
+print (choices)
