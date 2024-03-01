47d14218f79e62cc12f0231ed414ab289a2bde65
KekarLamar <jcarlos.demarcos@gmail.com>
2024-03-01 19:50:43 +0100

    Empeando otra parte del tutorial

--- Beginners.py
+++ Beginners.py
@@ -1,37 +1,5 @@
 import random
+#this is  a comment
 
-def getChoices():
-
-    player_choice = input ("Enter a choice (rock, paper, scissors: ")
-    options = ["rock", "paper", "scissors"]
-    computer_choice = random.choice(options)
-    choices = {"player": player_choice , "computer" : computer_choice}
-    return choices
-
-def checkWin (player, computer):
-    print ( f"You chose {player} , computer chose {computer}")
-    if player == computer:
-        return "It's a tie"
-    elif player == "rock":
-        if computer == "scissors":
-            return "Player wins"
-        else:
-            return "Paper covers rock! You lose."
-        
-    elif player == "paper":
-            if computer == "rock":
-                return "Player wins"
-            else:
-                return "Scissor cuts paper! You lose."
-
-    elif player == "scissors":
-                if computer == "paper":
-                    return "Player wins"
-                else:
-                    return "Rock smashes scissors! You lose."
-
-choices = getChoices()
-result = checkWin (choices["player"],choices["computer"])
-print(result)
-print(choices)
+name = "Beau"; print(name) # También se puede comentar aquí
 
